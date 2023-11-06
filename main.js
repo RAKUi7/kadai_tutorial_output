45af90a474d8a87b8c29942643a5f2930a70e2f6
RAKUi7 <rakuraku210@gmail.com>
2023-11-06 20:12:53 +0900

    4章テキストを表示しよう

---
+++ main.js
@@ -0,0 +1,57 @@
+let untyped = "";
+
+const untypedfield = document.getElementById("untyped");
+
+const textLists = [
+  "Hello World",
+  "This is my App",
+  "How are you?",
+  "Today is sunny",
+  "I love JavaScript!",
+  "Good morning",
+  "I am Japanese",
+  "Let it be",
+  "Samurai",
+  "Typing Game",
+  "Information Technology",
+  "I want to be a programmer",
+  "What day is today?",
+  "I want to build a web app",
+  "Nice to meet you",
+  "Chrome Firefox Edge Safari",
+  "machine learning",
+  "Brendan Eich",
+  "John Resig",
+  "React Vue Angular",
+  "Netscape Communications",
+  "undefined null NaN",
+  "Thank you very much",
+  "Google Apple Facebook Amazon",
+  "ECMAScript",
+  "console.log",
+  "for while if switch",
+  "var let const",
+  "Windows Mac Linux iOS Android",
+  "programming",
+];
+
+// ランダムなテキストを表示
+const createText = () => {
+  let random = Math.floor(Math.random() * textLists.length);
+
+  untyped = textLists[random];
+  untypedfield.textContent = untyped;
+};
+createText();
+
+// キー入力の判定
+const keyPress = () => {};
+
+// タイピングスキルのランクを判定
+const rankCheck = () => {};
+
+// ゲームを終了
+const gameOver = () => {};
+
+// カウントダウンタイマー
+const timer = () => {};
